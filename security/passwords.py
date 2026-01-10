a563b66d01fceec5408d33841b1e8e70da3ad687
Olexii <babiy0617@gmail.com>
2026-01-10 15:42:07 +0200

    Add funcs for hash and verify passwords

---
+++ security/passwords.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+    bcrypt__rounds=14
+)
+
+def hash_password(raw_password: str) -> str:
+    return pwd_context.hash(raw_password)
+
+
+def verify_password(raw_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(raw_password, hashed_password)
