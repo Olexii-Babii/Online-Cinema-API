4b4043382672c9e55f7d964429adb6d5d5f52d54
Olexii <babiy0617@gmail.com>
2026-01-11 14:00:42 +0200

    Update engine to async version

--- database/engine.py
+++ database/engine.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./movies.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./movies.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
