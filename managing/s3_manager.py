ef221eeb2b6e7c28000642ec7aa714b34fa15841
Olexii <babiy0617@gmail.com>
2026-01-14 15:36:13 +0200

    Update error with exception

--- managing/s3_manager.py
+++ managing/s3_manager.py
@@ -24,15 +24,15 @@ class S3Client():
                     "s3", endpoint_url=self.settings.S3_STORAGE_ENDPOINT
             ) as client:
                 await client.put_object(
-                    Bucket=self.settings.S3_STORAGE_BUCKET,
+                    Bucket=self.settings.S3_BUCKET_NAME,
                     Key=file_name,
                     Body=file_data,
                     ContentType="image/jpeg",
                 )
         except (ConnectionError, HTTPClientError, NoCredentialsError) as e:
             raise ConnectionError(f"Failed to connect to S3 storage: {str(e)}")
-        except BotoCoreError as e:
-            raise BotoCoreError(f"Failed to upload to S3 storage: {str(e)}")
+        except Exception as e:
+            raise ConnectionError(f"Failed to upload to S3 storage: {str(e)}")
 
     async def get_file_url(self, file_name: str) -> str:
         return f"{self.settings.S3_STORAGE_ENDPOINT}/{self.settings.S3_BUCKET_NAME}/{file_name}"
