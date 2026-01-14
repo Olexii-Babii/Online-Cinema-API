325302f7b7da47f364bdaa3268b055693353b21c
Olexii <babiy0617@gmail.com>
2026-01-14 15:58:34 +0200

    Add profile route

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+
 from routes.accounts import router as accounts_router
+from routes.profiles import router as profiles_router
+
 app = FastAPI()
 
 app.include_router(accounts_router, prefix=f"/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix=f"/profiles", tags=["profiles"])
