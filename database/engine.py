5aa4cf64abfe31fc9b7be862935eff3869780271
Olexii <babiy0617@gmail.com>
2026-01-21 13:53:58 +0200

    Add get_db_contextmanager&reset_database funcs to engine.py

--- database/engine.py
+++ database/engine.py
@@ -1,7 +1,10 @@
+from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+
+from database import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./movies.db"
 
@@ -19,3 +22,15 @@ AsyncSessionLocal = sessionmaker(
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
+
+
+@asynccontextmanager
+async def get_sqlite_db_contextmanager() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+async def reset_sqlite_database() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
