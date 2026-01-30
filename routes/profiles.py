622fb492d9b6dcd6f078c949607912831f351da4
Olexii <babiy0617@gmail.com>
2026-01-30 13:54:48 +0200

    Add documentation for profiles routes

--- routes/profiles.py
+++ routes/profiles.py
@@ -17,7 +17,41 @@ router = APIRouter()
 @router.post(
     "/users/{user_id}/profile/",
     response_model=ProfileResponseSchema,
+    summary="Create user profile",
+    description="User profile creation",
     status_code=status.HTTP_201_CREATED,
+    responses={
+                 400: {
+                     "description": "Bad Request - User already has a profile.",
+                     "content": {
+                         "application/json": {
+                             "example": {
+                                 "detail": "User already has a profile."
+                             }
+                         }
+                     },
+                 },
+                 401: {
+                     "description": "Unauthorized - User not found or not active.",
+                     "content": {
+                         "application/json": {
+                             "example": {
+                                 "detail": "User not found or not active."
+                             }
+                         }
+                     },
+                 },
+                 403: {
+                     "description": "Forbidden - User can not create profile for another user.",
+                     "content": {
+                         "application/json": {
+                             "example": {
+                                 "detail": "You don't have permission to edit this profile."
+                             }
+                         }
+                     },
+                 },
+    }
 )
 async def create_user_profile(
     user_id: int,
@@ -97,7 +131,13 @@ async def create_user_profile(
             detail="An error occurred while creating profile.",
         )
 
-@router.get("/me", response_model=ProfileResponseSchema)
+
+@router.get("/me",
+            response_model=ProfileResponseSchema,
+            status_code=status.HTTP_200_OK,
+            summary="User`s profile",
+            description=("Allows the user to obtain information about themselves"),
+            )
 async def read_me(current_user: ProfileBaseSchema = Depends(get_current_user),
                   db: AsyncSession = Depends(get_db)):
     return current_user
