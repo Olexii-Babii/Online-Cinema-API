84f584a96262ce3740a451a4285d068579717b3c
Olexii <babiy0617@gmail.com>
2026-02-02 11:11:54 +0200

    Update SYNC_DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,10 @@ class Settings(BaseAppSettings):
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
-    SYNC_DATABASE_URL: str = os.getenv("SYNC_DATABASE_URL", "postgres://test_user:test_password@localhost:5432/test_db")
+
+    @property
+    def SYNC_DATABASE_URL(self):
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_DB_PORT}/{self.POSTGRES_DB}"
 
     SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
