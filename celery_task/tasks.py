4591ac5a7c84e471cd5bfc5d31d66dffbe656508
Olexii <babiy0617@gmail.com>
2026-02-02 14:55:39 +0200

    Add chech for testing

--- celery_task/tasks.py
+++ celery_task/tasks.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine, delete
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -8,9 +10,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-settings = get_settings()
-sync_engine = create_engine(settings.SYNC_DATABASE_URL)
-SyncSessionLocal = sessionmaker(bind=sync_engine, class_=Session)
+if os.getenv("ENVIRONMENT") != "testing":
+    settings = get_settings()
+    sync_engine = create_engine(settings.SYNC_DATABASE_URL)
+    SyncSessionLocal = sessionmaker(bind=sync_engine, class_=Session)
 
 @celery_app.task(name="delay_delete_activation_token")
 def delay_delete_activation_token(token_id: int):
